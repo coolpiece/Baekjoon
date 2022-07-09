e472037d510963706ad6e69074ee0ac1250cb01c
coolpiece <90741842+coolpiece@users.noreply.github.com>
2022-07-10 05:59:50 +0900

    [Silver V] Title: 숫자 카드, Time: 216 ms, Memory: 8296 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <utility>
+using namespace std;
+
+int main(void) {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(0);
+
+	int n, m;
+	vector<int> v1, v2;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int num;
+		cin >> num;
+		v1.push_back(num);
+	}
+	cin >> m;
+	for (int i = 0; i < m; i++) {
+		int num;
+		cin >> num;
+		v2.push_back(num);
+	}
+	sort(v1.begin(), v1.end());
+	for (int i = 0; i < m; i++) {
+		if (binary_search(v1.begin(), v1.end(), v2[i]))
+			cout << "1" << " ";
+		else
+			cout << "0" << " ";
+	}
+}
