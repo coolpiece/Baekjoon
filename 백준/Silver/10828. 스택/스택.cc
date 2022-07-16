f992ba13c2461987493b363738603589dc1ac15f
coolpiece <90741842+coolpiece@users.noreply.github.com>
2022-07-17 04:17:36 +0900

    [Silver IV] Title: 스택, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.cc
+++ 백준/Silver/10828. 스택/스택.cc
@@ -2,16 +2,14 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <utility>
-#include <set>
-#include <map>
+#include <stack>
 using namespace std;
 
 int main(void) {
 	cin.tie(NULL);
 	ios_base::sync_with_stdio(0);
 
-	vector<int> stack;
+	stack<int> s;
 	int n;
 	cin >> n;
 	for (int i = 0; i < n; i++) {
@@ -20,28 +18,28 @@ int main(void) {
 		if (!input.compare("push")) {
 			int x;
 			cin >> x;
-			stack.push_back(x);
+			s.push(x);
 		}
 		else if (!input.compare("pop")) {
-			if (stack.size() == 0)
+			if (s.size() == 0)
 				cout << -1 << '\n';
 			else {
-				cout << stack.back() << '\n';
-				stack.pop_back();
+				cout << s.top() << '\n';
+				s.pop();
 			}
 		}
 		else if (!input.compare("size")) {
-			cout << stack.size() << '\n';
+			cout << s.size() << '\n';
 		}
 		else if (!input.compare("empty")) {
-			if (stack.size() == 0) cout << 1 << '\n';
+			if (s.size() == 0) cout << 1 << '\n';
 			else cout << 0 << '\n';
 		}
 		else if (!input.compare("top")) {
-			if (stack.size() == 0)
+			if (s.size() == 0)
 				cout << -1 << '\n';
 			else
-				cout << stack.back() << '\n';
+				cout << s.top() << '\n';
 		}
 	}
 	
