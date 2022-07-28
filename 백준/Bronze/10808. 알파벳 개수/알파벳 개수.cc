fce4a0ef0e7f4eab4ee1de2a4b422bd84bf5856c
coolpiece <90741842+coolpiece@users.noreply.github.com>
2022-07-28 21:11:35 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <stack>
+using namespace std;
+
+int main(void) {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(0);
+
+	string str;
+	stack<pair<char, int>> s;
+	vector<int> n(26);
+	cin >> str;
+	for (int i = 0; i < str.length(); i++) {
+		int idx = str[i] - 'a';
+		n[idx]++;
+	}
+	for (int i = 0; i < 26; i++)
+		cout << n[i] << " ";
+}
