b0daf565e82fb17f2d39ec5ca8ef698ea753faba
coolpiece <90741842+coolpiece@users.noreply.github.com>
2022-09-17 02:34:18 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main (void) {
+    int n, i, j;
+    scanf("%d", &n);
+    for(i=1; i<n+1; i++) {
+        for(j=1; j<i+1; j++)
+            printf("*");
+        printf("\n");
+    }
+    return 0;
+}
