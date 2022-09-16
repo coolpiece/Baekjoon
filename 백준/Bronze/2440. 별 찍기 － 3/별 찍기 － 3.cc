878ee3a74e181a6fed63ae5adf4e364f0d0ecee5
coolpiece <90741842+coolpiece@users.noreply.github.com>
2022-09-17 02:36:31 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(0);
+	
+	int n;
+	cin >> n;
+	for (int i = n; i > 0; i--) {
+		for (int j = 0; j < i; j++) {
+			cout << "*";
+		}
+		cout << '\n';
+	}
+}
