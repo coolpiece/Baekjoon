93786fc7829e1f1d7fb60a4a408bd977b6211b76
coolpiece <90741842+coolpiece@users.noreply.github.com>
2023-08-14 16:43:18 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(0);
+
+    string str;
+    cin >> str;
+    int i;
+    cin >> i;
+    cout << str[i - 1];
+}
