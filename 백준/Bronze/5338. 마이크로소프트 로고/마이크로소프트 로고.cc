b07c0b05d1f6c4223f9b6ac0e1c56fcfae473562
coolpiece <90741842+coolpiece@users.noreply.github.com>
2024-04-21 02:59:38 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(0);
+    cout << "       _.-;;-._\n"
+         << "'-..-'|   ||   |\n"
+         << "'-..-'|_.-;;-._|\n"
+         << "'-..-'|   ||   |\n"
+         << "'-..-'|_.-''-._|\n";
+}
