2868e9db564de9668db02df58c581eb59e380713
coolpiece <90741842+coolpiece@users.noreply.github.com>
2024-04-23 13:31:41 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 132 ms, Memory: 28868 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.cc
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.cc
@@ -2,25 +2,28 @@
 using namespace std;
 
 int main(void) {
-    cin.tie(NULL);
     ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
     string str;
     int q;
     cin >> str >> q;
 
-    vector<vector<int>> v(str.size(), vector<int>(26));
-    for (int i = 0; i < str.size(); i++) { // 누적합 계산.
-        int idx = str[i] - 'a'; // 벡터에 알파벳이 나타난 횟수 저장.
-        for (int j = i; j < str.size(); j++) {
-            v[j][idx]++;
+    int length = str.size();
+    vector<vector<int>> v(length + 1, vector<int>(26));
+    for (int i = 1; i <= length; i++) { // 누적합 계산.
+        int idx = str[i - 1] - 'a';    // 벡터에 알파벳이 나타난 횟수 저장.
+        for (int j = 0; j < 26; j++) {
+            v[i][j] = v[i - 1][j];
         }
+        v[i][idx]++;
     }
     for (int i = 0; i < q; i++) {
         char a;
         int l, r;
         cin >> a >> l >> r;
-        if(!l) cout << v[r][a - 'a'] << '\n'; // l이 0인 경우.
-        else cout << v[r][a - 'a'] - v[l - 1][a - 'a'] << '\n';
+        int idx = a - 'a';
+        cout << v[r + 1][idx] - v[l][idx] << '\n';
     }
 }
