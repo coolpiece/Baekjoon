b76c21e004b5ff7f92a1fb6163859754c27b31d2
coolpiece <90741842+coolpiece@users.noreply.github.com>
2024-06-07 03:43:33 +0900

    [Silver I] Title: IOIOI, Time: 12 ms, Memory: 4196 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.cc
+++ 백준/Silver/5525. IOIOI/IOIOI.cc
@@ -6,17 +6,33 @@ int main(void) {
     cin.tie(NULL);
 
     int n, m, cnt = 0;
-    string p = "IOI", s;
+    string s;
     cin >> n >> m >> s;
-    for (int i = 1; i < n; i++) p.append("OI");
-    while(!s.empty()) {
+
+    vector<int> v;
+    for (int i = 0; i < m; i++) {
+        int len = 1;
         string::size_type idx;
-        idx = s.find(p);
-        if (string::npos == idx) break;
-        else {
-            cnt++;
-            s = s.substr(idx + 1);
+
+        idx = s.find("IOI", i);
+        if (string::npos == idx)
+            break;
+        else
+            idx += 2;
+
+        while (idx < m - 2) {
+            if (s[idx + 1] == 'O' && s[idx + 2] == 'I') {
+                len++;
+                idx += 2;
+            } else
+                break;
         }
+        i = idx;
+        v.push_back(len);
+    }
+
+    for (int i = 0; i < v.size(); i++) {
+        if (v[i] >= n) cnt += v[i] - n + 1;
     }
     cout << cnt;
 }
