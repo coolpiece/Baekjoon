341d525ec33973a39fcb5b84586710837c0a573c
coolpiece <90741842+coolpiece@users.noreply.github.com>
2024-06-07 11:25:09 +0900

    [Bronze IV] Title: Buying in Bulk, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/26332. Buying in Bulk/Buying in Bulk.cc
+++ 백준/Bronze/26332. Buying in Bulk/Buying in Bulk.cc
@@ -12,13 +12,10 @@ int main(void) {
         int c, p;
         cin >> c >> p;
 
-        int price = c * p;
-        int discount = (c - 1) * 2;
-        price -= discount;
-        // int price = p;
-        // if (c > 2) {
-        //     price += (p - 2) * (c - 1);
-        // }
+        int price = p;
+        if (c >= 2) {
+            price += (p - 2) * (c - 1);
+        }
         cout << c << " " << p << '\n' << price << '\n';
     }
 }
