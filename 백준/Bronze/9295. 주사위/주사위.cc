4d5952f30d815d3b8f5fdb75fff3db0762fb1dc6
coolpiece <90741842+coolpiece@users.noreply.github.com>
2024-07-12 02:40:30 +0900

    [Bronze IV] Title: 주사위, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9295. 주사위/주사위.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    for (int i = 1; i <= t; i++) {
+        int a, b;
+        cin >> a >> b;
+        cout << "Case " << i << ": " << a + b << '\n';
+    } }
