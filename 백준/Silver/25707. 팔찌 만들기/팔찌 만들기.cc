848d505202607668f3853a01fe39346058b9a65b
coolpiece <90741842+coolpiece@users.noreply.github.com>
2024-08-28 02:06:48 +0900

    [Silver V] Title: 팔찌 만들기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/25707. 팔찌 만들기/팔찌 만들기.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, len = 0, a, b;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) cin >> v[i];
+    sort(v.begin(), v.end());  // 정렬.
+    cout << 2 * (v[n - 1] - v[0]);
+}
