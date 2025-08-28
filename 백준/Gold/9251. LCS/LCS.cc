cb7f638a329f501da68e5c9bab165c104e40e6a1
coolpiece <90741842+coolpiece@users.noreply.github.com>
2025-08-28 19:45:08 +0900

    [Gold V] Title: LCS, Time: 4 ms, Memory: 6004 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    string str1, str2;
+    cin >> str1 >> str2;
+    int len1 = str1.size(), len2 = str2.size();
+    vector<vector<int>> dp(len1 + 1, vector<int>(len2 + 1));
+
+    for (int i = 1; i <= len1; i++) {
+        for (int j = 1; j <= len2; j++) {
+            if (str1[i - 1] == str2[j - 1])
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            else
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        }
+    }
+    cout << dp[len1][len2];
+}
