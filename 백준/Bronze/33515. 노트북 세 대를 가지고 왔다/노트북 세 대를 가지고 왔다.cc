1b31729be50a7067a1de64b2d5711e34d146f54d
coolpiece <90741842+coolpiece@users.noreply.github.com>
2025-09-24 02:19:18 +0900

    [Bronze V] Title: 노트북 세 대를 가지고 왔다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/33515. 노트북 세 대를 가지고 왔다/노트북 세 대를 가지고 왔다.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t1, t2;
+    cin >> t1 >> t2;
+    cout << min(t1, t2);
+}
