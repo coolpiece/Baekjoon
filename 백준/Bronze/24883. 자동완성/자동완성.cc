d5b76ab598e1217f777eb1a20b542d06325e4b2d
coolpiece <90741842+coolpiece@users.noreply.github.com>
2025-10-24 03:30:18 +0900

    [Bronze V] Title: 자동완성, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    char c;
+    cin >> c;
+    if (c == 'N' || c == 'n') cout << "Naver D2";
+    else
+        cout << "Naver Whale";
+}
