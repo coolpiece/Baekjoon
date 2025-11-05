49ad1a4320edb02aa3f25fb7f6c08916ca488bc8
coolpiece <90741842+coolpiece@users.noreply.github.com>
2025-11-06 03:38:50 +0900

    [Silver V] Title: K번째 수, Time: 1008 ms, Memory: 21552 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) cin >> a[i];
+    sort(a.begin(), a.end());
+    cout << a[k - 1];
+}
