128871f6b19bd8040d06d18fee25ba9abdd36cd1
coolpiece <90741842+coolpiece@users.noreply.github.com>
2025-12-02 03:38:09 +0900

    [Bronze II] Title: 방 배정하기, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/14697. 방 배정하기/방 배정하기.cc
+++ 백준/Bronze/14697. 방 배정하기/방 배정하기.cc
@@ -5,23 +5,20 @@ int main(void) {
   cin.tie(NULL);
   ios_base::sync_with_stdio(false);
 
-  int a, b, c, n, cnt, flag = 0;
+  int a, b, c, n, flag = 0;
   cin >> a >> b >> c >> n;
-  cnt = n - n / a * a;
-  for (int i = 0; i < n / a; i++) {
-    for (int j = 0; j < cnt / b; j++) {
-      cnt = cnt - cnt / b * b;
-      if (cnt == 0 || cnt % c == 0) {
-        flag = 1;
-        break;
-      } else
-        cnt += b;
-    }
-    if (cnt == 0) {
+
+  for (int i = 0; n - a * i >= 0; i++) {
+    if (n - a * i == 0) {
       flag = 1;
       break;
     }
-    cnt += a;
+    for (int j = 0; n - a * i - b * j >= 0; j++) {
+      if (n - a * i - b * j == 0 || (n - a * i - b * j) % c == 0) {
+        flag = 1;
+        break;
+      }
+    }
   }
   if (flag)
     cout << 1;
