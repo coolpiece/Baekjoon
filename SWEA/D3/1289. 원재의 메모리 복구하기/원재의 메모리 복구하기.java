15c61f0290063af3959ceb196ab3753c5011c9ba
coolpiece <90741842+coolpiece@users.noreply.github.com>
2026-02-02 11:37:36 +0900

    [D3] Title: 원재의 메모리 복구하기, Time: 85 ms, Memory: 24,192 KB -BaekjoonHub

--- SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.java
+++ SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.java
@@ -1,24 +1,20 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
+import java.io.*;
+ 
 public class Solution {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
-		for (int t = 1; t <= T; t++) {
-			String input = br.readLine();
-			char cur = '0';
-			int cnt = 0;
-			for (int i = 0; i < input.length(); i++) {
-				if (input.charAt(i) != cur) {
-					cnt++;
-					if (cur == '0')
-						cur = '1';
-					else
-						cur = '0';
-				}
-			}
-			System.out.println("#" + t + " " + cnt);
-		}
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 1; t <= T; t++) {
+            String input = br.readLine();
+            char cur = '0';
+            int cnt = 0;
+            for (int i = 0; i < input.length(); i++) {
+                if (input.charAt(i) != cur) {
+                    cnt++;
+                    cur = (cur == '0') ? '1' : '0';
+                }
+            }
+            System.out.println("#" + t + " " + cnt);
+        }
+    }
 }
