0fc9614f03ad7b89856df20fbee5d727cca1f4d1
coolpiece <90741842+coolpiece@users.noreply.github.com>
2026-02-04 16:58:21 +0900

    [D4] Title: [S/W 문제해결 기본] 2일차 - Ladder1, Time: 136 ms, Memory: 38,124 KB -BaekjoonHub

--- SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.java
+++ SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.java
@@ -7,26 +7,26 @@ public class Solution {
 
 		for (int t = 1; t <= 10; t++) {
 			int tc = Integer.parseInt(br.readLine()), endX = 99, endY = 0;
-			String[][] map = new String[100][];
+			char[][] map = new char[100][100];
 			for (int i = 0; i < 100; i++) { // 입력 값 map에 저장.
-				map[i] = br.readLine().split(" ");
-				for (int j = 0; j < 100; j++) {
-					if (map[i][j].equals("2")) // 도착점 좌표 저장.
+				String str = br.readLine();
+				for (int j = 0, idx = 0; j < 100; j++, idx += 2) {
+					map[i][j] = str.charAt(idx);
+					if (i == 99 && map[i][j] == '2') // 도착점 좌표 저장.
 						endY = j;
 				}
 			}
 			while (true) {
 				if (endX == 0)
 					break;
-
-				while (endY - 1 >= 0 && map[endX][endY - 1].equals("1")) { // 왼쪽으로 이동.
+				while (endY - 1 >= 0 && map[endX][endY - 1] == '1') { // 왼쪽으로 이동.
 					endY--;
-					if (endX - 1 >= 0 && map[endX - 1][endY].equals("1"))
+					if (endX - 1 >= 0 && map[endX - 1][endY] == '1')
 						endX--;
 				}
-				while (endY + 1 < 100 && map[endX][endY + 1].equals("1")) { // 오른쪽으로 이동.
+				while (endY + 1 < 100 && map[endX][endY + 1] == '1') { // 오른쪽으로 이동.
 					endY++;
-					if (endX - 1 >= 0 && map[endX - 1][endY].equals("1"))
+					if (endX - 1 >= 0 && map[endX - 1][endY] == '1')
 						endX--;
 				}
 				if (endX - 1 >= 0)
