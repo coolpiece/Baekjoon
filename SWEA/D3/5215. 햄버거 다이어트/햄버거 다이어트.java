fbe9b6f0a2ca83dc9d68a70daabeb8be08f806b3
coolpiece <90741842+coolpiece@users.noreply.github.com>
2026-02-06 13:42:20 +0900

    [D3] Title: 햄버거 다이어트, Time: 1,249 ms, Memory: 30,948 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -8,19 +8,28 @@ public class Solution {
 		int T = Integer.parseInt(br.readLine());
 		for (int t = 1; t <= T; t++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int n = Integer.parseInt(st.nextToken()), limit = Integer.parseInt(st.nextToken());
-			int[][] dp = new int[n + 1][limit + 1]; // dp[재료 번호][최대 칼로리]
-			for (int i = 1; i <= n; i++) {
+			int n = Integer.parseInt(st.nextToken()), limit = Integer.parseInt(st.nextToken()), maxVal = 0;
+			List<int[]> list = new ArrayList<>();
+			for (int i = 0; i < n; i++) {
 				st = new StringTokenizer(br.readLine());
 				int score = Integer.parseInt(st.nextToken()), k = Integer.parseInt(st.nextToken());
-				for (int j = 1; j <= limit; j++) {
-					if (j >= k)
-						dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - k] + score);
-					else
-						dp[i][j] = dp[i - 1][j];
-				}
+				list.add(new int[] { score, k });
 			}
-			rst.append("#").append(t).append(" ").append(dp[n][limit]).append("\n");
+			for (int i = 0; i < (1 << n); i++) {
+				int totalScore = 0, totalKcal = 0;
+				for (int j = 0; j < n; j++) {
+					if ((i & (1 << j)) != 0) {
+						totalScore += list.get(j)[0];
+						totalKcal += list.get(j)[1];
+					}
+					if (totalKcal > limit) {
+						totalScore = 0;
+						break;
+					}
+				}
+				maxVal = Math.max(maxVal, totalScore);
+			}
+			rst.append("#").append(t).append(" ").append(maxVal).append("\n");
 		}
 		System.out.println(rst);
 	}
