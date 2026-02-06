4ce4d5f9ab1baf00a689d141aad3db34ecb01a26
coolpiece <90741842+coolpiece@users.noreply.github.com>
2026-02-06 09:57:20 +0900

    [D3] Title: 햄버거 다이어트, Time: 126 ms, Memory: 37,932 KB -BaekjoonHub

---
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder rst = new StringBuilder(100);
+		int T = Integer.parseInt(br.readLine());
+		for (int t = 1; t <= T; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int n = Integer.parseInt(st.nextToken()), limit = Integer.parseInt(st.nextToken());
+			int[][] dp = new int[n + 1][limit + 1]; // dp[재료 번호][최대 칼로리]
+			for (int i = 1; i <= n; i++) {
+				st = new StringTokenizer(br.readLine());
+				int score = Integer.parseInt(st.nextToken()), k = Integer.parseInt(st.nextToken());
+				for (int j = 1; j <= limit; j++) {
+					if (j >= k)
+						dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - k] + score);
+					else
+						dp[i][j] = dp[i - 1][j];
+				}
+			}
+			rst.append("#").append(t).append(" ").append(dp[n][limit]).append("\n");
+		}
+		System.out.println(rst);
+	}
+}
