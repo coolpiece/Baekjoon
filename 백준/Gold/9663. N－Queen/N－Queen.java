89181eb0e1c44f992f8bd51ad6000c9c92c48dc3
coolpiece <90741842+coolpiece@users.noreply.github.com>
2026-02-11 01:39:10 +0900

    [Gold IV] Title: N-Queen, Time: 2352 ms, Memory: 14860 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,14 +1,14 @@
 import java.io.*;
 
 public class Main {
-	private static boolean[] row, leftupDia, rightupDia;
+	private static boolean[] col, leftupDia, rightupDia;
 	private static int n, cnt;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		n = Integer.parseInt(br.readLine());
 		cnt = 0;
-		row = new boolean[n];
+		col = new boolean[n];
 		leftupDia = new boolean[n * 2 - 1]; // 왼쪽 위로 가는 대각선.
 		rightupDia = new boolean[n * 2 - 1]; // 오른쪽 위로 가는 대각선.
 		setQueen(0);
@@ -21,14 +21,14 @@ public class Main {
 			return;
 		}
 		for (int i = 0; i < n; i++) {
-			if (!row[i]) {
+			if (!col[i]) {
 				if (rightupDia[x + i])
 					continue;
 				if (leftupDia[x - i + n - 1])
 					continue;
-				row[i] = rightupDia[x + i] = leftupDia[x - i + n - 1] = true;
+				col[i] = rightupDia[x + i] = leftupDia[x - i + n - 1] = true;
 				setQueen(x + 1);
-				row[i] = rightupDia[x + i] = leftupDia[x - i + n - 1] = false;
+				col[i] = rightupDia[x + i] = leftupDia[x - i + n - 1] = false;
 			}
 		}
 	}
