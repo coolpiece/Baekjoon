ceec44c7f8bf35f3ef4367458b214c6d1f1b25b9
coolpiece <90741842+coolpiece@users.noreply.github.com>
2026-02-11 00:22:04 +0900

    [Gold IV] Title: N-Queen, Time: 2608 ms, Memory: 14816 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -0,0 +1,35 @@
+import java.io.*;
+
+public class Main {
+	private static boolean[] row, leftupDia, rightupDia;
+	private static int n, cnt;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
+		cnt = 0;
+		row = new boolean[n];
+		leftupDia = new boolean[n * 2 - 1]; // 왼쪽 위로 가는 대각선.
+		rightupDia = new boolean[n * 2 - 1]; // 오른쪽 위로 가는 대각선.
+		setQueen(0);
+		System.out.println(cnt);
+	}
+
+	public static void setQueen(int x) {
+		if (x == n) {
+			cnt++;
+			return;
+		}
+		for (int i = 0; i < n; i++) {
+			if (!row[i]) {
+				if (rightupDia[x + i])
+					continue;
+				if (leftupDia[x - i + n - 1])
+					continue;
+				row[i] = rightupDia[x + i] = leftupDia[x - i + n - 1] = true;
+				setQueen(x + 1);
+				row[i] = rightupDia[x + i] = leftupDia[x - i + n - 1] = false;
+			}
+		}
+	}
+}
