746889c29254b7615bb5a3ee03912b596d02d72f
coolpiece <90741842+coolpiece@users.noreply.github.com>
2026-02-17 01:46:39 +0900

    [Bronze V] Title: Hello Judge, Time: 168 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for (int i = 1; i <= n; i++)
+			System.out.println("Hello World, Judge " + i + "!");
+	}
+}
