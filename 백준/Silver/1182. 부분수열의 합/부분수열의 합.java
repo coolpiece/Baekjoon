8f1a9b0a6396b30410800294fb566d0d6ac972ed
coolpiece <90741842+coolpiece@users.noreply.github.com>
2026-03-16 12:16:29 +0900

    [Silver II] Title: 부분수열의 합, Time: 124 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int n, s, cnt, select[], list[];
+	static int n, s, cnt, list[];
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -10,28 +10,61 @@ public class Main {
 		n = Integer.parseInt(st.nextToken());
 		s = Integer.parseInt(st.nextToken());
 		cnt = 0;
-		select = new int[n];
 		list = new int[n];
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < n; i++)
 			list[i] = Integer.parseInt(st.nextToken());
-		for (int i = 1; i <= n; i++)
-			combi(0, 0, i);
+		subset(0, 0);
+		if (s == 0)
+			cnt--;
 		System.out.println(cnt);
 	}
 
-	static void combi(int idx, int count, int pickNum) {
-		if (count == pickNum) {
-			int sum = 0;
-			for (int i = 0; i < pickNum; i++)
-				sum += select[i];
+	static void subset(int idx, int sum) {
+		if (idx == n) {
 			if (sum == s)
 				cnt++;
 			return;
 		}
-		for (int i = idx; i < n; i++) {
-			select[count] = list[i];
-			combi(i + 1, count + 1, pickNum);
-		}
+		subset(idx + 1, sum + list[idx]); // 포함하는 경우.
+		subset(idx + 1, sum); // 포함하지 않는 경우.
 	}
 }
+
+//import java.io.*;
+//import java.util.*;
+//
+//public class Main {
+//	static int n, s, cnt, select[], list[];
+//
+//	public static void main(String[] args) throws Exception {
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		StringTokenizer st = new StringTokenizer(br.readLine());
+//		n = Integer.parseInt(st.nextToken());
+//		s = Integer.parseInt(st.nextToken());
+//		cnt = 0;
+//		select = new int[n];
+//		list = new int[n];
+//		st = new StringTokenizer(br.readLine());
+//		for (int i = 0; i < n; i++)
+//			list[i] = Integer.parseInt(st.nextToken());
+//		for (int i = 1; i <= n; i++)
+//			combi(0, 0, i);
+//		System.out.println(cnt);
+//	}
+//
+//	static void combi(int idx, int count, int pickNum) {
+//		if (count == pickNum) {
+//			int sum = 0;
+//			for (int i = 0; i < pickNum; i++)
+//				sum += select[i];
+//			if (sum == s)
+//				cnt++;
+//			return;
+//		}
+//		for (int i = idx; i < n; i++) {
+//			select[count] = list[i];
+//			combi(i + 1, count + 1, pickNum);
+//		}
+//	}
+//}
