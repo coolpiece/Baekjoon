cd8e2ee3c20f6c1d6f6d83719e241af73aa62e75
coolpiece <90741842+coolpiece@users.noreply.github.com>
2026-03-25 03:34:33 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 48 ms, Memory: 8288 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
@@ -21,20 +21,22 @@ int main() {
     edge[a].emplace_back(b, c);
     edge[b].emplace_back(a, c);
   }
-  vector<bool> visited(v, false);
+  vector<bool> visited(v + 1, false);
   priority_queue<Edge> pq;
   pq.emplace(1, 0);
-  int cnt = -1;
+  int cnt = 0;
   long long total = 0;
   while (!pq.empty()) {
-    if (cnt == v - 1) break;
     Edge cur = pq.top();  // 가중치가 가장 작은 간선 선택.
     int next = pq.top().to;
     pq.pop();
     if (visited[next]) continue;  // 이미 방문한 정점이면 패스.
     visited[next] = true;
-    cnt++;
     total += cur.weight;
+    cnt++;
+
+    if (cnt == v) break; // 정점이 모두 연결된 경우.
+      
     for (int i = 0; i < edge[next].size();
          i++) {  // 연결 가능한 간선 후보 추가.
       pq.emplace(edge[next][i].to, edge[next][i].weight);
