a8bb943788f3f311d2050d6ff179c7cf6dd92e31
coolpiece <90741842+coolpiece@users.noreply.github.com>
2026-04-14 15:49:49 +0900

    [D4] Title: [Professional] 키 순서, Time: 2,247 ms, Memory: 103,120 KB -BaekjoonHub

--- SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -10,50 +10,41 @@ public class Solution {
 		for (int t = 1; t <= T; t++) {
 			rst.append("#").append(t).append(" ");
 			int n = Integer.parseInt(br.readLine()), m = Integer.parseInt(br.readLine());
-			int tall[][] = new int[n][n]; // tall[i][j]: i보다 j가 더 크다.
-			int small[][] = new int[n][n]; // small[i][j]: i보다 j가 더 작다.
+			int student[][] = new int[n][n]; // student[i][j]: i보다 j가 더 크다.
 			// 자신을 제외한 모든 사람이 자신보다 큰 지, 작은 지 알면 자신의 순서 알 수 있음.
 
-			for (int i = 0; i < n; i++) {
-				Arrays.fill(tall[i], Integer.MAX_VALUE / 2);
-				Arrays.fill(small[i], Integer.MAX_VALUE / 2);
-			}
-			for (int i = 0; i < n; i++) {
-				tall[i][i] = 0;
-				small[i][i] = 0;
-			}
+			for (int i = 0; i < n; i++)
+				Arrays.fill(student[i], Integer.MAX_VALUE / 2);
+			for (int i = 0; i < n; i++)
+				student[i][i] = 0;
+
 			for (int i = 0; i < m; i++) {
 				st = new StringTokenizer(br.readLine());
 				int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
-				tall[a - 1][b - 1] = 1;
-				small[b - 1][a - 1] = 1;
+				student[a - 1][b - 1] = 1;
 			}
 
 			for (int k = 0; k < n; k++) {
 				for (int i = 0; i < n; i++) {
 					for (int j = 0; j < n; j++) {
-						tall[i][j] = Math.min(tall[i][k] + tall[k][j], tall[i][j]); // i < k < j.
-						small[j][i] = Math.min(small[j][k] + small[k][i], small[j][i]); // j > k > i.
+						student[i][j] = Math.min(student[i][k] + student[k][j], student[i][j]); // i < k < j.
 					}
 				}
 			}
 
-//			for (int i = 0; i < n; i++) {
-//				System.out.println(Arrays.toString(tall[i]));
-//			}
-//			System.out.println();
-//			for (int i = 0; i < n; i++) {
-//				System.out.println(Arrays.toString(small[i]));
-//			}
+//			for (int i = 0; i < n; i++)
+//				System.out.println(Arrays.toString(student[i]));
 
 			int cnt = 0; // 자신의 순서를 알 수 있는 학생 수.
 			for (int i = 0; i < n; i++) {
-				int tmp = 0; // i보다 큰 지, 작은 지 알 수 있는 사람 수.
+				boolean flag = true;
 				for (int j = 0; j < n; j++) {
-					if (tall[i][j] != Integer.MAX_VALUE / 2 || small[i][j] != Integer.MAX_VALUE / 2)
-						tmp++;
+					if (student[i][j] == Integer.MAX_VALUE / 2 && student[j][i] == Integer.MAX_VALUE / 2) {
+						flag = false;
+						break;
+					}
 				}
-				if (tmp == n)
+				if (flag)
 					cnt++;
 			}
 			rst.append(cnt).append("\n");
