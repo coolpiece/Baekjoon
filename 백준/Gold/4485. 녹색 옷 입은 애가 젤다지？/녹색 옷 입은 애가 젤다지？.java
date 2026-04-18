fa43c014d401dea14b705209e50d53e35318b8ea
coolpiece <90741842+coolpiece@users.noreply.github.com>
2026-04-18 23:16:48 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 192 ms, Memory: 20244 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -24,7 +24,7 @@ public class Main {
 		StringBuilder rst = new StringBuilder();
 
 		int t = 1, n, board[][], cost[][], r, c, size;
-		boolean visited[][];
+//		boolean visited[][];
 		PriorityQueue<Rupee> pq = new PriorityQueue<>(); // 선택 가능한 위치 후보. 비용이 적은 순으로 정렬.
 		while (true) {
 			n = Integer.parseInt(br.readLine());
@@ -32,7 +32,7 @@ public class Main {
 				break;
 			board = new int[n][n];
 			cost = new int[n][n];
-			visited = new boolean[n][n];
+//			visited = new boolean[n][n];
 			for (int i = 0; i < n; i++) // 최댓값으로 초기화.
 				Arrays.fill(cost[i], Integer.MAX_VALUE);
 
@@ -42,29 +42,45 @@ public class Main {
 					board[i][j] = Integer.parseInt(st.nextToken());
 			}
 
+			cost[0][0] = board[0][0];
 			pq.add(new Rupee(0, 0, board[0][0])); // 시작점 넣기.
 
 			while (!pq.isEmpty()) {
 				r = pq.peek().r;
 				c = pq.peek().c;
 				size = pq.poll().size;
-				if (visited[r][c])
-					continue; // 이미 선택한 위치.
 
-				visited[r][c] = true; // 방문 처리.
-				cost[r][c] = size; // 최소 비용 업데이트.
+				if (r == n - 1 && c == n - 1) { // (추가) 이미 목적지에 도달함. 조기 종료.
+					cost[r][c] = size;
+					break;
+				}
+//				if (visited[r][c])
+//					continue; // 이미 선택한 위치.
+//
+//				visited[r][c] = true; // 방문 처리.
+//				cost[r][c] = size; // 최소 비용 업데이트.
+
+				if (size > cost[r][c]) // 이미 더 짧은 경로를 찾은 경우 패스.
+					continue;
 
 				for (int i = 0; i < 4; i++) { // 상하좌우 탐색. 아직 선택하지 않은 위치 추가.
 					int nr = r + dr[i], nc = c + dc[i];
 					if (nr < 0 || nr >= n || nc < 0 || nc >= n)
 						continue;
-					if (visited[nr][nc]) // 비용 계산이 끝난 위치는 다시 계산할 필요 없음. (가장 최소인 값만 갱신했기 때문에 더 작아질 수 x.)
-						continue;
-					pq.add(new Rupee(nr, nc, size + board[nr][nc]));
+//					if (visited[nr][nc]) // 비용 계산이 끝난 위치는 다시 계산할 필요 없음. (가장 최소인 값만 갱신했기 때문에 더 작아질 수 x.)
+//						continue;
+
+					// (추가) 이미 찾은 경로보다 더 적을 때만 큐에 넣음.
+					int nextSize = size + board[nr][nc];
+					if (cost[nr][nc] > nextSize) {
+						cost[nr][nc] = nextSize; // 미리 업데이트.
+						pq.add(new Rupee(nr, nc, nextSize));
+					}
 				}
 			}
 			rst.append("Problem ").append(t).append(": ").append(cost[n - 1][n - 1]).append("\n");
 			t++;
+			pq.clear();
 		}
 		System.out.println(rst);
 	}
